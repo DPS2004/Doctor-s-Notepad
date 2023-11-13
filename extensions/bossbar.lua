10b3a3fb6eb3260addcecab1effaaab127654954
9thCore <38700349+9thCore@users.noreply.github.com>
2023-11-13 08:01:44 +0200

    fix bossbar tag name discrepancy lol

--- extensions/bossbar.lua
+++ extensions/bossbar.lua
@@ -156,8 +156,8 @@ local extension = function(_level)
 			local prefix = 'dn.bossbar(' .. room .. ')'
 			local onmiss = '[onMiss]dn.bossbar(' .. room .. ')'
 			local onhit = '[onHit]dn.bossbar(' .. room .. ')'
-			local updatepatienthp = 'dn.bossbar(' .. room .. ').patientupdate'
-			local updatevirushp = 'dn.bossbar(' .. room .. ').virushp'
+			local updatepatienthp = 'dn.bossbar(' .. room .. ').patientupdatehp'
+			local updatevirushp = 'dn.bossbar(' .. room .. ').virusupdatehp'
 			
 			local stillalivecond = level:customconditional(prefix .. '.stillalive', patienthpvar .. ' > 0')
 			stillalivecond:red(true)
